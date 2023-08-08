bf8f4431af241c7fa1b41a3f6ccf3f0f76d8164c
Armanc <94117770+4rmanc@users.noreply.github.com>
2023-08-09 02:43:14 +0300

    Update polynomial.rs

--- src/kzg_variables/polynomial.rs
+++ src/kzg_variables/polynomial.rs
@@ -7,16 +7,20 @@ use halo2::{
 };
 use rand::thread_rng;
 
+/// Polynomial Structure
 #[derive(Clone, Debug)]
 pub struct Polynomial {
+    // Coefficients of the polynomial
     pub(crate) coefficients: Vec<Fr>,
 }
 
 impl Polynomial {
+    /// Creates a new polynomial from given vector
     pub fn new(coefficients: Vec<Fr>) -> Self {
         Self { coefficients }
     }
 
+    /// Creates a new random polynomial with given length
     pub fn random(length: usize) -> Self {
         let mut random_polynomial_coeff = Vec::new();
         let rng = &mut thread_rng();
@@ -28,6 +32,7 @@ impl Polynomial {
         }
     }
 
+    /// Evaluates polynomial on the given value
     pub fn eval(&self, x: &Fr) -> Fr {
         let mut eval = Fr::ZERO;
         let mut point = Fr::ONE;
@@ -38,25 +43,32 @@ impl Polynomial {
         eval
     }
 
-    pub fn quotient_calculator_for_kzg(&self, rhs: Self) -> Self {
-        // Will divide lhs to rhs with perfect division operation.
-        // If it is not a perfect division and rhs == degree 1, won't work.
+    /// Calculates quotient using long division algorithm
+    pub fn long_division(&self, rhs: Self) -> Self {
+        // Will divide lhs to rhs with long division operation (Only divides perfect divisions).
+        // Checks if rhs == degree 1 polynomial.
         assert!(rhs.coefficients.len() == 2);
-        // Biggest degree of the quotient will carry same coefficient from the self
-        // Here assigning it in reverse order and at the end of the program we will make it correct
+
+        // Here assigning it in reverse order and at the end of the function it will be reverted.
         let mut coefficients = self.coefficients.clone();
         coefficients.reverse();
+
+        // Biggest degree of the quotient will always be the biggest degree's coefficient from
+        // the numerator polynomial. That is why it is assigned here
         let mut quotient: Vec<Fr> = vec![coefficients[0]];
 
         // Long division algorithm for degree == 1 divisor
         for i in 0..coefficients.len() - 2 {
             quotient.push(coefficients[i + 1] - quotient[i] * rhs.coefficients[0]);
         }
+
+        // Revert quotient to correct positioning for the whole algorithm
         quotient.reverse();
 
         Polynomial::new(quotient)
     }
 
+    /// Makes the commitment for the given polynomial
     pub fn commitment(&self, srs_1: &Vec<G1Affine>) -> G1Affine {
         let mut commitment_c1 = G1::default();
         for i in 0..self.coefficients.len() {
